1d54e10175ad0a96a876692c3cd02b1578304202
Stefan van der Merwe <44154511+Sniperlyf3@users.noreply.github.com>
2026-09-12 01:33:15 +0200

    Pipe Windows private key to tailcat over stdin

--- cmd/meowshell/exec_windows.go
+++ cmd/meowshell/exec_windows.go
@@ -3,21 +3,50 @@ package main
 import (
 	"errors"
 	"fmt"
+	"io"
 	"os"
 	"os/exec"
-	"time"
 )
-
-const stagedKeyGracePeriod = time.Second
 
 func runTailcat(bin string, argv, environ []string) error {
 	cmd := exec.Command(bin, argv[1:]...)
 	cmd.Env = environ
-	cmd.Stdin, cmd.Stdout, cmd.Stderr = os.Stdin, os.Stdout, os.Stderr
+	cmd.Stdout, cmd.Stderr = os.Stdout, os.Stderr
+
+	keyData := takeStagedKey()
+	defer zeroBytes(keyData)
+
+	var keyStdin io.WriteCloser
+	if len(keyData) != 0 {
+		var err error
+		keyStdin, err = cmd.StdinPipe()
+		if err != nil {
+			return fmt.Errorf("opening tailcat key stdin: %w", err)
+		}
+	} else {
+		cmd.Stdin = os.Stdin
+	}
 
 	if err := cmd.Start(); err != nil {
-		cleanupStagedKey()
+		if keyStdin != nil {
+			_ = keyStdin.Close()
+		}
 		return err
+	}
+
+	if keyStdin != nil {
+		if _, err := keyStdin.Write(keyData); err != nil {
+			_ = keyStdin.Close()
+			_ = cmd.Process.Kill()
+			_ = cmd.Wait()
+			return fmt.Errorf("sending private key to tailcat: %w", err)
+		}
+		if err := keyStdin.Close(); err != nil {
+			_ = cmd.Process.Kill()
+			_ = cmd.Wait()
+			return fmt.Errorf("closing tailcat key stdin: %w", err)
+		}
+		zeroBytes(keyData)
 	}
 
 	// Windows has no PR_SET_PDEATHSIG equivalent. Put tailcat in a
@@ -33,13 +62,7 @@ func runTailcat(bin string, argv, environ []string) error {
 	}
 	defer job.Close()
 
-	go func() {
-		time.Sleep(stagedKeyGracePeriod)
-		cleanupStagedKey()
-	}()
-
 	err := cmd.Wait()
-	cleanupStagedKey()
 
 	var exit *exec.ExitError
 	if errors.As(err, &exit) {
