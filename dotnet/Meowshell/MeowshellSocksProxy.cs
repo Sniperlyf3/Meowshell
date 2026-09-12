9337edda265c101d3b8e29a662fb2435f92fcf88
Stefan van der Merwe <44154511+Sniperlyf3@users.noreply.github.com>
2026-09-12 12:55:15 +0200

    Validate HOME before launching MeowshellSocksProxy.cs

--- dotnet/Meowshell/MeowshellSocksProxy.cs
+++ dotnet/Meowshell/MeowshellSocksProxy.cs
@@ -47,7 +47,7 @@ public sealed class MeowshellSocksProxy : IAsyncDisposable
         TimeSpanValidation.EnsurePositiveAndBounded(options.GracePeriod, nameof(options.GracePeriod));
 
         var (meowshell, tailcat) = MeowshellBinaries.Locate(options.BinaryDirectory, options.Naming);
-        Directory.CreateDirectory(options.HomeDirectory);
+        MeowshellHomeDirectory.EnsureSecure(options.HomeDirectory);
 
         var psi = new ProcessStartInfo
         {
